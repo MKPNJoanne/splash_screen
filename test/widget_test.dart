073c48b8733c35232e50c2dd514660f01f7580e0
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-07-20 09:58:59 +0530

    create splash_screen.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:splash_screen/main.dart';
+import 'package:splash_screen/splash_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
